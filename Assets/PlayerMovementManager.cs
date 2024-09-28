37d1686e12bbdd4fcd8de016d21178970f7cf53e
Andrei <49598916+AndreiGama@users.noreply.github.com>
2024-09-28 10:25:01 +0100

    Implemented gravity

--- Assets/PlayerMovementManager.cs
+++ Assets/PlayerMovementManager.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class PlayerMovementManager : MonoBehaviour {
 	CharacterController charController;
+	[SerializeField] float gravity = -9.81f;
     [SerializeField] float moveSpeed;
+
+	[SerializeField] Transform groundCheck;
+	[SerializeField] float groundDistance;
+	[SerializeField] LayerMask groundMask;
+
+	Vector3 velocity;
+	bool isGrounded;
 	private void Awake() {
 		charController = GetComponent<CharacterController>();
 	}
 
 	private void Update() {
+		isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+		if (isGrounded && velocity.y < 0) {
+			velocity.y = -2f;
+		}
 		float x = Input.GetAxis("Horizontal");
 		float z = Input.GetAxis("Vertical");
 		Vector3 move = transform.right * x + transform.forward * z;
 		charController.Move(move * moveSpeed * Time.deltaTime);
+
+		velocity.y += gravity * Time.deltaTime;
+		charController.Move(velocity * Time.deltaTime);
 	}
 }
